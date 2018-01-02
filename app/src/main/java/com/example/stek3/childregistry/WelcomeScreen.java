b83486db6bee316b7aa183be429b6e6dc44bce3d
SsebaddukaG <luckygilbert1993@gmail.com>
2018-01-03 00:24:45 +0300

    Login and splash screen inclusions

---
+++ app/src/main/java/com/example/stek3/childregistry/WelcomeScreen.java
@@ -0,0 +1,30 @@
+package com.example.stek3.childregistry;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+
+        Thread myThread = new Thread()
+        {
+            @Override
+            public void run() {
+                try {
+                    sleep(4000);
+                    Intent  intent = new Intent(getApplicationContext(),LoginScreen.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
+}
